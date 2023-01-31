a32a2850a3a826a3decf3e7aa7b997ee78145cac
Victor Miguel Torres <vtorresquintanilla0206@gmail.com>
2023-01-31 10:41:06 -0600

    add: auth Provider to main app

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { AuthProvider } from './contexts/AuthContext';
+
 import AuthLayout from './layout/AuthLayout';
 
 const router = createBrowserRouter([
@@ -13,6 +16,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
