e3984a92147a4e09cf16a13b79df7ab6e6669c82
Victor Miguel Torres <vtorresquintanilla0206@gmail.com>
2023-01-31 12:55:05 -0600

    add: components to admin routes

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,35 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 
 import AuthLayout from './layout/AuthLayout';
+import AdminLayout from './layout/AdminLayout';
+import NewProduct from './components/admin/NewProduct';
+import ProductList from './components/admin/ProductList';
+import NewFactura from './components/admin/newFactura';
+import FacturaList from './components/admin/FacturaList';
 
 const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AdminLayout/>,
+    children: [
+      {
+        index: true,
+        element: <NewProduct/>
+      },
+      {
+        path: '/admin/ver-productos',
+        element: <ProductList/>
+      },
+      {
+        path: '/admin/crear-factura',
+        element: <NewFactura/>
+      },
+      {
+        path: '/admin/ver-facturas',
+        element: <FacturaList/>
+      },
+    ],
+  },
   {
     path: '/auth',
     element: <AuthLayout/>
