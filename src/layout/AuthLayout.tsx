15b43795fdfc35f963d45fc08cea2cb1399edb25
Victor Miguel Torres <vtorresquintanilla0206@gmail.com>
2023-01-30 20:57:41 -0600

    change: body class to auth layout section tag

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -1,7 +1,11 @@
 import Tabs from '../components/Tabs'
 
 function AuthLayout() {
-  return <Tabs/>
+  return (
+    <section className='min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-sky-500 to-indigo-500'>
+      <Tabs/>
+    </section>
+  )
 }
 
 export default AuthLayout
