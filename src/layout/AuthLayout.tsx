fa7c11eab38721851d8ce904e13847a4efe5a983
Victor Miguel Torres <vtorresquintanilla0206@gmail.com>
2023-01-30 18:30:22 -0600

    add: layout for auth section

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -1,12 +1,7 @@
-import React from 'react'
 import Tabs from '../components/Tabs'
 
 function AuthLayout() {
-  return (
-    <section className='min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-sky-500 to-indigo-500'>
-      <Tabs/>
-    </section>
-  )
+  return <Tabs/>
 }
 
 export default AuthLayout
