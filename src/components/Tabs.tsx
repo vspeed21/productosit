eaf62c82c8d41bb0d29ae426cd7d4b9e5acde5ac
Victor Miguel Torres <vtorresquintanilla0206@gmail.com>
2023-01-30 19:37:25 -0600

    fix: tabs & form design

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -6,17 +6,17 @@ function Tabs() {
   const [tabNumber, setTabNumber] = useState(1);
 
   return (
-    <div className='bg-white rounded-md h-40 p-3 w-72'>
+    <div className='bg-white rounded-md p-3 sm:w-96'>
       <h2 className="font-bold text-center text-xl my-3">{tabNumber === 1 ? 'Inicia sesión' : 'Crea una cuenta'}</h2>
       <ul className="flex flex-col md:flex-row gap-2 justify-center">
         <li
-          className={`capitalize hover:bg-indigo-800 transition-colors duration-300 hover:cursor-pointer py-1 px-2 rounded ${tabNumber === 1 ? 'bg-indigo-600 text-white' : 'text-black hover:text-white'}`}
+          className={`capitalize text-center hover:bg-indigo-800 transition-colors duration-300 hover:cursor-pointer py-1 px-2 rounded ${tabNumber === 1 ? 'bg-indigo-600 text-white' : 'text-black hover:text-white'}`}
           onClick={() => setTabNumber(1)}
         >
           Iniciar sesion
         </li>
         <li
-          className={`capitalize hover:bg-indigo-800 transition-colors duration-300 hover:cursor-pointer py-1 px-2 rounded ${tabNumber === 2 ? 'bg-indigo-600 text-white' : 'text-black hover:text-white'}`}
+          className={`capitalize text-center hover:bg-indigo-800 transition-colors duration-300 hover:cursor-pointer py-1 px-2 rounded ${tabNumber === 2 ? 'bg-indigo-600 text-white' : 'text-black hover:text-white'}`}
           onClick={() => setTabNumber(2)}
         >
             Crear cuenta
