5d70711c93d54f2347f71ad396fddfdcd07f43e2
Victor Miguel Torres <vtorresquintanilla0206@gmail.com>
2023-02-03 15:38:11 -0600

    create: ProductF interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -11,11 +11,19 @@ export interface Numeros {
   totalPagar: number
 }
 
+export interface ProductoF {
+  id: string | number
+  name: string
+  price: string
+  stock: number
+  cantidadP: number
+}
+
 export interface Factura {
   numeroFactura?: number
   client: string
   factura: string
-  productoFactura: any
+  productoFactura: ProductoF[]
   numeros: Numeros
   fecha: number
 }
