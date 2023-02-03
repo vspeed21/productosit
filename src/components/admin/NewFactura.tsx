59a6a906ba3c3e179a13bf2723bba658fbbce10c
Victor Miguel Torres <vtorresquintanilla0206@gmail.com>
2023-02-03 10:44:32 -0600

    create: all function to NewFactura

--- src/components/admin/NewFactura.tsx
+++ src/components/admin/NewFactura.tsx
@@ -10,47 +10,53 @@ import { Product } from '../../interfaces';
 function NewFactura() {
   const [client, setClient] = useState('');
   const [factura, setFactura] = useState('');
-  const [nombresP, setNombresP] = useState<string[]>([]);
+  const [buscadorPro, setbuscadorPro] = useState('');
 
-
-  const [inputValues, setInputValues] = useState([
-    { id: Date.now(), name: '', price: '', stock: 1 },
+  const [productoFactura, setproductoFactura] = useState([
+    { id: Date.now(), name: '', price: '', stock: 1, cantidadP: 0 },
   ]);
+
+  const [subtotal, setSubtotal] = useState(0);
+  const [impuesto, setImpuesto] = useState(0);
+  const [totalPagar, setTotalPagar] = useState(0);
+
+  const [cantidad, setCantidad] = useState(1);
 
   const { products } = useProduct();
 
-  function findProduct() {
-    for (let i = 0; i < nombresP.length; i++) {
-      for (let j = 0; j < products.length; j++) {
-        if(nombresP[i] === products[j].name) {
-          inputValues[i].price = products[j]?.price;
-          inputValues[i].stock = Number(products[j]?.stock);
-        }
-      }
+  useEffect(() => {
+    setImpuesto(subtotal * 0.15);
+    setTotalPagar(subtotal + impuesto);
+  }, [subtotal, impuesto, totalPagar])
+
+  useEffect(() => {
+    let valor = 0;
+    for (let i = 0; i < productoFactura.length; i++) {
+      valor = valor + Number(productoFactura[i].price) * productoFactura[i].cantidadP
     }
-  }
+    setSubtotal(valor);
+  }, [subtotal, productoFactura]);
 
-  useEffect(()  => {
-    findProduct();
-  }, [inputValues]);
+  const productoEncontrado = products.filter(pro => pro.name === buscadorPro);
+  const addProductF = () => {
 
-  const addInput = () => {
-    setInputValues([
-      ...inputValues,
-      { id: Date.now(), name: '', price: '', stock: 1 }
+    if (cantidad > Number(productoEncontrado[0]?.stock)) {
+      alert('no se puede');
+      return;
+    }
+
+    setproductoFactura([
+      ...productoFactura,
+      { id: Date.now(), name: productoEncontrado[0].name, price: productoEncontrado[0].price, stock: Number(productoEncontrado[0].stock), cantidadP: cantidad }
     ]);
+    setbuscadorPro('');
+    setCantidad(1);
   }
 
-  function revomeInput(i: number) {
-    const newInputValues = [...inputValues];
-    newInputValues.splice(i, 1);
-    setInputValues(newInputValues);
-  }
-
-  const changeInput = (e: ChangeEvent<HTMLInputElement>, i: number) => {
-    let newInputValues = [...inputValues];
-    newInputValues[i] = { ...newInputValues[i], [e.target.name]: [e.target.value] };
-    setInputValues(newInputValues);
+  function removeProFactura(i: number) {
+    const newproductoFactura = [...productoFactura];
+    newproductoFactura.splice(i, 1);
+    setproductoFactura(newproductoFactura);
   }
 
   const handleSubmit = (e: FormEvent) => {
@@ -102,71 +108,134 @@ function NewFactura() {
           </div>
         </div>
 
-        <button onClick={addInput} type='button'>Add</button>
+        <button onClick={addProductF} type='button'>Add</button>
 
-        {inputValues.map((input, i) => (
-          <div className='flex flex-col gap-4 mb-5' key={i}>
-            <div className='flex flex-col md:flex-row gap-4 justify-around'>
-              <div className='flex flex-col gap-3'>
-                <label className='text-gray-800 uppercase font-bold'>
-                  {`Producto ${i + 1}`}
-                </label>
-                <input
-                  list='productos'
-                  type='list'
-                  name='name'
-                  value={input.name || ''}
-                  onChange={e => {
-                    changeInput(e, i);
-                    setNombresP([
-                      ...nombresP,
-                      e.target.value,
-                    ]);
-                    findProduct();
-                  }}
-                  className='bg-gray-100 p-1 pl-3 outline-none focus:outline-blue-600 focus:outline-2 rounded focus:shadow border border-gray-400 focus:border-none'
-                />
 
-                <datalist
-                  id='productos'
-                >
-                  {products.length && products.map((pro, index) => (
-                    <option
-                      value={pro.name}
-                      key={index}
-                    />
-                  ))}
-                </datalist>
-              </div>
+        <div className='flex flex-col gap-3'>
+          <label className='text-gray-800 uppercase font-bold'>
+            Buscar productos
+          </label>
+          <input
+            list='productos'
+            type='list'
+            name='name'
+            value={buscadorPro}
+            onChange={e => setbuscadorPro(e.target.value)}
+            className='bg-gray-100 p-1 pl-3 outline-none focus:outline-blue-600 focus:outline-2 rounded focus:shadow border border-gray-400 focus:border-none'
+          />
 
-              <div className='flex flex-col gap-3'>
-                <label>price</label>
-                <input
-                  type='text'
-                  name='price'
-                  value={input.price}
-                  onChange={e => changeInput(e, i)}
-                  disabled={true}
-                  readOnly={true}
-                  className='bg-gray-100 p-1 pl-3 outline-none focus:outline-blue-600 focus:outline-2 rounded focus:shadow border border-gray-400 focus:border-none text-center'
-                />
-              </div>
+          {productoEncontrado[0]?.price && (
+            <div className='flex gap-3 justify-evenly my-3'>
+              <p className='border-b-2 border-blue-500'>Cantidad disponible: {productoEncontrado[0]?.stock}</p>
+              <p className='border-b-2 border-blue-500'>Precio: {formatearCantidad(productoEncontrado[0]?.price)}</p>
             </div>
-            <label>stock</label>
-            <input
-              type='number'
-              name='stock'
-              value={input.stock || 1}
-              onChange={e => changeInput(e, i)}
-              className='bg-gray-100 p-1 pl-3 outline-none focus:outline-blue-600 focus:outline-2 rounded focus:shadow border border-gray-400 focus:border-none'
-            />
-            {
-              i >= 1 && <button onClick={() => revomeInput(i)} type='button'>remove</button>
-            }
+          )}
+
+          <div className='flex mb-4 gap-5 justify-center'>
+            <button
+              type='button'
+              className='-mt-5'
+              onClick={() => {
+                if (cantidad <= 1) return;
+                if (cantidad > 1) {
+                  setCantidad(cantidad - 1)
+                }
+              }}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+
+            </button>
+            <div className='flex flex-col gap-3'>
+
+              <input
+                id='cantidad'
+                type='number'
+                name='stock'
+                value={cantidad}
+                disabled={!buscadorPro ? true : false}
+                onChange={e => setCantidad(Number(e.target.value))}
+                min={1}
+                className='bg-gray-100 p-1 pl-3 outline-none focus:outline-blue-600 focus:outline-2 rounded focus:shadow border border-gray-400 focus:border-none text-center w-30'
+              />
+            </div>
+            <button
+              type='button'
+              className='-mt-5'
+              onClick={() => {
+                if (cantidad >= Number(productoEncontrado[0].stock) ) return;
+                if (cantidad >= 1) {
+                  setCantidad(cantidad + 1)
+                }
+              }}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+
+            </button>
           </div>
-        )
+
+          <datalist
+            id='productos'
+          >
+            {products.length && products.map((pro, index) => (
+              <option
+                value={pro.name}
+                key={index}
+              />
+            ))}
+          </datalist>
+        </div>
+
+        {productoFactura.length > 1 ? (
+          <table className='w-full mt-10 table-auto shadow bg-white mr-5 overflow-y-scroll'>
+            <thead className='bg-blue-800 text-white'>
+              <tr>
+                <th className="p-2">Nombre</th>
+                <th className="p-2">Precio</th>
+                <th className="p-2">Cantidad</th>
+                <th className='p-2'>Acciones</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {productoFactura.length >= 1 && productoFactura.map((input, i) => (
+                <tr className='border-b hover:bg-gray-100 text-center' key={input.id}>
+                  {input.name !== '' && <td className='p-3'>{input.name}</td>}
+                  {input.name !== '' && (
+                    <td className='p-3'>{formatearCantidad(input.price)}</td>
+                  )}
+                  {input.name !== '' && (
+                    <td className='p-3'>
+                      {input.cantidadP}
+                    </td>)}
+                  {input.name !== '' && <button type='button' onClick={() => removeProFactura(i)}>eliminar</button>}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p className='text-center font-bold text-lg my-5'>No hay productos para facturar</p>
         )}
 
+        {subtotal ? (
+          <div className='flex gap-5 justify-end my-5'>
+            <p>
+              Subtotal: {''}
+              <span className='font-bold'>{formatearCantidad(subtotal.toString())}</span>
+            </p>
+            <p>
+              Impuesto: {''}
+              <span className='font-bold'>{formatearCantidad(impuesto.toString())}</span>
+            </p>
+            <p>
+              Total a Pagar: {''}
+              <span className='font-bold'>{formatearCantidad(totalPagar.toString())}</span>
+            </p>
+          </div>
+        ) : null}
 
         <div className='flex justify-center md:justify-end'>
           <input
